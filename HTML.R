bae100fb6de9441f1c2abf3de7c58b8e4d1dfba4
le402n38\uer <joannabuhuang@gmail.com>
2023-12-06 16:30:45 +0800

    擷取 title 資訊

--- HTML.R
+++ HTML.R
@@ -5,3 +5,14 @@ txt = scan(URL, what = "character", encoding = "UTF-8", quiet = TRUE)
 
 head(txt, 15)
 
+#透過 paste 將換行的字串轉為一個常字串
+txt_new = paste(txt, sep = "", collapse = " ")
+TITLE.pos = gregexpr("<title>.*</title>", txt_new)
+
+#擷取資訊 (頭跟尾的位置)
+start.TITLE.pos = TITLE.pos[[1]][1]
+end.TITLE.pos = start.TITLE.pos + attr(TITLE.pos[[1]], "match.length")[1] - 1
+
+TITLE.word = substr(txt_new, start.TITLE.pos, end.TITLE.pos)
+
+TITLE.word
